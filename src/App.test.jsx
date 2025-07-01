a11fb9647dd75b66ca0f7dd19b2a75f3de5f859a
root <root@DESKTOP-TPU7DIA.localdomain>
2025-07-01 12:23:37 +0000

    project adding first time

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom';
+
+test('renders app', () => {
+  render(<App />);
+  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+});
