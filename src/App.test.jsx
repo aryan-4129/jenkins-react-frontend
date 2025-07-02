59f65c02eca6b7154f9efd852ead18f5b0a3d265
aryan-4129 <150704582+aryan-4129@users.noreply.github.com>
2025-07-02 11:09:41 +0530

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';  // 👈 Add this!
 import App from './App';
 import '@testing-library/jest-dom';
 
 test('renders app', () => {
   render(<App />);
-  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
 });
+i
