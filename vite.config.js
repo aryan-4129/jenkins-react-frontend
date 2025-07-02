b824a2a757f789f0bb29e0aee2c3b548a0765247
aryan-4129 <150704582+aryan-4129@users.noreply.github.com>
2025-07-02 11:11:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // 👈 This allows global `expect`, `test`, etc.
+    environment: 'jsdom', // 👈 Required for DOM testing
+  },
+});
